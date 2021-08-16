c3a93de3e46b702b598a967589c8fbe390a96415
jackjackE <tmddjs210@naver.com>
2021-08-16 16:16:27 +0900

    26. Scaffolding a Flutter App

---
+++ i_am_rich/i_am_rich/android/app/src/main/kotlin/com/mtjin/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mtjin.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- i_am_rich/i_am_rich/lib/main.dart
+++ i_am_rich/i_am_rich/lib/main.dart
@@ -4,8 +4,17 @@ import 'package:flutter/material.dart';
 // main() 은 모든 플러터앱의 시작점이다.
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
-    ),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image( // NetworkImage는 Url을 통해 서버이미지를 가져올때 사용합니다. 안드로이드의 Glide라고 생각!, 그밖에 AssetImage 등 다른것들도 있습니다.
+          image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnYB7pV4de12zrKQayds0fU56TBYw06fYaqQ&usqp=CAU'),
+        ),
+      ),
+    )
   ));
 }
