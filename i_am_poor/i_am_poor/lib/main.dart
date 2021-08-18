80f7da0d53c6ec7cccda7f3a6ccc64ea885e3c06
jackjackE <tmddjs210@naver.com>
2021-08-18 12:20:02 +0900

    섹션 5: I Am Poor - App Challenge 프로젝트 완성

--- i_am_poor/i_am_poor/lib/main.dart
+++ i_am_poor/i_am_poor/lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        title: Text('I am poor :('),
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/pale-bank-1.png'),
+        ),
+      ),
+    ),
+  ));
 }
