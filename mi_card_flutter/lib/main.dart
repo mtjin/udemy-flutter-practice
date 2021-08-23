166297828e6512863e5fbc7154634de48633546d
jackjackE <tmddjs210@naver.com>
2021-08-24 00:07:52 +0900

    40. Hot Reload and Hot Restart - Flutter Power Tools

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.lime,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
