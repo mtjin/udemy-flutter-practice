5794d346d85eec2d1d28c5165f6d8c8bd8ea1996
jackjackE <tmddjs210@naver.com>
2021-09-10 21:11:11 +0900

    42. How to use Column & Row Widgets for Layout

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,16 +13,28 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.lime,
         body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.fromLTRB(100.0, 60.0, 10.0, 20.0), // left, top, right, bottom 각각 마진설정
-            //margin : EdgeInsets.all(30.0) // 전체 한번에 마진설정
-            //margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 50.0), // 수직, 수평 마진 설정
-            //margin: EdgeInsets.only(left: 30.0), // 한 방향만 마진 설정
-            padding: EdgeInsets.all(10.0),
-            height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            child: Text("안녕하세요"),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  height: 100.0,
+                  color: Colors.white,
+                  child: Text("컨테이너 1")
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.blue,
+                child: Text("컨테이너 2"),
+              ),
+              Container(
+                  height: 100.0,
+                  color: Colors.orange,
+                  child: Text("컨테이너 3")
+              ),
+            ],
           ),
         ),
       ),
