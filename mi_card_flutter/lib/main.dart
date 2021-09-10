7e4a3e40f5a6b889f748a072a3800047ea458bfb
jackjackE <tmddjs210@naver.com>
2021-09-10 22:01:41 +0900

    44. Tapping into Widget Properties

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,25 +13,21 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // 컨테이너를 끝에서부터 동등하게 간격조절하여 가운데 정렬
-            children: [
-              Container(
-                //height 는 match parent 자동으로 되게 설정 따로 안해줘도됨
-                width: 100.0,
-                color: Colors.red,
-              ),
-              Container( // 반반 나눠서 색 2개 만듬, 근데 더 좋은 방법있을거같은데 흠..
-                  width: 100.0,
-                  height: 200.0,
-                  color: Colors.yellow,
-                  padding: EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
-                  child: Container(color: Colors.green)),
-              Container(width: 100.0, color: Colors.blue),
-            ],
-          ),
-        ),
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/profile.jpg'),
+            ),
+            Text(
+              "JackJackE",
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        )),
       ),
     );
   }
