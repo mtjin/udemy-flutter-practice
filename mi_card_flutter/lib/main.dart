8784e7c7138dcd93b9f839c403e4c8ae27e769a8
jackjackE <tmddjs210@naver.com>
2021-09-13 18:05:51 +0900

    47. Flutter Card & ListTile Widgets

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -14,80 +14,75 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/profile.jpg'),
+          mainAxisAlignment: MainAxisAlignment.center,
+          // Column(main) 기준으로 가운데 정렬
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/profile.jpg'),
+            ),
+            Text(
+              "JackJackE",
+              style: TextStyle(
+                  fontFamily: 'Pacifico', // pubspec.yaml 에 폰트이름 설정해논거 세팅
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              // Divider 를 사용하여 쉽게 구분선을 추가
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Text(
+              "Flutter Developer",
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  // pubspec.yaml 에 폰트이름 설정해논거 세팅
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
                 ),
-                Text(
-                  "JackJackE",
+                title: Text(
+                  '010-1234-5678',
                   style: TextStyle(
-                      fontFamily: 'Pacifico', // pubspec.yaml 에 폰트이름 설정해논거 세팅
-                      fontSize: 40.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "Flutter Developer",
-                  style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      // pubspec.yaml 에 폰트이름 설정해논거 세팅
                       fontSize: 20.0,
-                      color: Colors.teal.shade100,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold),
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro'),
                 ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 25.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        '010-1234-5678',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
-                  ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
                 ),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 25.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'mtjin@github.com',
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.teal.shade900,
-                            fontFamily: 'Source Sans Pro'),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            )),
+                title: Text(
+                  'mtjin@github.com',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro'),
+                ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
