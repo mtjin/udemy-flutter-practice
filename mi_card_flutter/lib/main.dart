f5fb4403815d021759c31e97d1064a3207ef7952
jackjackE <tmddjs210@naver.com>
2021-09-13 18:05:16 +0900

    46. Adding Material Icons with the Icon Widget

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -14,31 +14,80 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/profile.jpg'),
-            ),
-            Text(
-              "JackJackE",
-              style: TextStyle(
-                  fontFamily: 'Pacifico', // pubspec.yaml 에 폰트이름 설정해논거 세팅
-                  fontSize: 40.0,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                Text(
+                  "JackJackE",
+                  style: TextStyle(
+                      fontFamily: 'Pacifico', // pubspec.yaml 에 폰트이름 설정해논거 세팅
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      // pubspec.yaml 에 폰트이름 설정해논거 세팅
+                      fontSize: 20.0,
+                      color: Colors.teal.shade100,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
                   color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Flutter Developer",
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro', // pubspec.yaml 에 폰트이름 설정해논거 세팅
-                fontSize: 40.0,
-                color: Colors.teal.shade100,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold
-              ),
-            )
-          ],
-        )),
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '010-1234-5678',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'mtjin@github.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
