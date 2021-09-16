4f6123146ee1b9ee7955244960be3315075be147
jackjackE <tmddjs210@naver.com>
2021-09-16 18:32:58 +0900

    53. Adding Gesture Detection with Flutter Button Widges

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -23,17 +23,21 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0), // 패딩설정
-              child: Image.asset('images/dice1.png'),
-            ), // image.asset 로 코드 간결화
-          ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              // FlatButton은 기본적으로 16 패딩을 갖음
+              onPressed: () {
+                print('왼쪽 주사위 onPressed!!!');
+              }, // 클릭이벤트(void callback 구조)
               child: Image.asset('images/dice1.png'),
             ),
+          ), // image.asset 로 코드 간결화
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                onPressed: () {
+                  print('오른쪽 주사위 onPressed!!!');
+                }, //
+                child: Image.asset('images/dice1.png')),
           )
         ],
       ),
