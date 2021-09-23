cba08e7da61b9f64d0c9edb207d2304ba8395048
jackjackE <tmddjs210@naver.com>
2021-09-23 19:58:51 +0900

    67. Step 2 - Create a Stateless Widget

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -5,3 +5,18 @@ void main() => runApp(
         home: null,
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('나에게 무엇이든 물어봐요'),
+        backgroundColor: Colors.blue[900],
+      ),
+    );
+  }
+}
