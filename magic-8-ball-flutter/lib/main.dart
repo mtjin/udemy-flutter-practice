4fff6ae162564e63b44322eeaba5f068633ec6b6
jackjackE <tmddjs210@naver.com>
2021-09-23 20:47:30 +0900

    70. Step 5 - Randomise it

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,11 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(
-      MaterialApp(
-        home: BallPage(),
-      ),
-    );
+void main() => runApp(
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
 
 class BallPage extends StatelessWidget {
   const BallPage({Key key}) : super(key: key);
@@ -31,13 +32,22 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void randomBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      print(ballNumber);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: FlatButton(
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
         onPressed: () {
-          print('I got clicked');
+          randomBall();
         },
       ),
     );
