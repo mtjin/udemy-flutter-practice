c01111983307eb5861a935c2a756b22bfad69ec1
jackjackE <tmddjs210@naver.com>
2022-04-03 17:33:59 +0900

    108. Step 1 - Adding a Image as a Background

--- destini-challenge-starting/ios/Flutter/flutter_export_environment.sh
+++ destini-challenge-starting/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Git\destini-challenge-starting"
+export "FLUTTER_APPLICATION_PATH=D:\Git\udemy-flutter-practice\destini-challenge-starting"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
