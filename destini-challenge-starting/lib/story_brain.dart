edd825d5d6af234930083634b8013fed78ba1cc8
jackjackE <tmddjs210@naver.com>
2022-04-03 18:44:09 +0900

    Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.

--- destini-challenge-starting/lib/story_brain.dart
+++ destini-challenge-starting/lib/story_brain.dart
@@ -43,9 +43,10 @@ class StoryBrain {
   String getStory() {
     return _storyData[0].storyTitle;
   }
-}
-
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
+  String getChoice1(){
+    return _storyData[0].choice1;
+  }
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
 
@@ -62,3 +63,4 @@ class StoryBrain {
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+}
