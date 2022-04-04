74f7bba6ca4176d7e71002f5f5e90838da93cd33
jackjackE <tmddjs210@naver.com>
2022-04-04 17:09:24 +0900

    124. [Dart] Final vs. Const

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,31 +23,37 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: activeCardColour,
                 )),
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: activeCardColour,
                 ))
               ],
             )),
             Expanded(
                 child: ReusableCard(
-              colour: Color(0xFF1D1E33),
+              colour: activeCardColour,
             )),
             Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: activeCardColour,
                 )),
                 Expanded(
                     child: ReusableCard(
-                  colour: Color(0xFF1D1E33),
+                  colour: activeCardColour,
                 ))
               ],
             )),
+            Container(
+              color: bottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
           ],
         ));
   }
