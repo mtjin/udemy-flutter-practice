be9fdbe0d0ffe599bdd3dd8cb5c9bce0d3120cfb
jackjackE <tmddjs210@naver.com>
2022-04-10 16:34:53 +0900

    133. Multi-Screen Apps Using Routes and Navigation

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,9 +1,11 @@
+import 'package:bmi_calculator/results_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'constants.dart';
 import 'icon_content.dart';
+import 'results_page.dart';
 import 'reusable_card.dart';
 
 enum Gender { male, female }
@@ -195,11 +197,18 @@ class _InputPageState extends State<InputPage> {
                 ))
               ],
             )),
-            Container(
-              color: kBottomContainerColour,
-              margin: EdgeInsets.only(top: 10.0),
-              width: double.infinity,
-              height: kBottomContainerHeight,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ResultsPage()));
+              },
+              child: Container(
+                child: Text("CALCULATE"),
+                color: kBottomContainerColour,
+                margin: EdgeInsets.only(top: 10.0),
+                width: double.infinity,
+                height: kBottomContainerHeight,
+              ),
             )
           ],
         ));
---
+++ bmi-calculator-flutter/lib/results_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CACULATOR'),
+      ),
+      body: Text('HELLO'),
+    );
+  }
+}
