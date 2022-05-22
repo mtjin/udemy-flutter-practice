a64897d4434476181dbe2e0e85a98f4c5a0e2445
jackjackE <tmddjs210@naver.com>
2022-05-22 16:34:18 +0900

    165. Level 2 - Silver Medal

--- bitcoin-flutter-silver-start/lib/coin_data.dart
+++ bitcoin-flutter-silver-start/lib/coin_data.dart
@@ -32,14 +32,14 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const coinAPIURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+const apiKey = 'CB66DB8B-23D2-439A-8CF0-EC819C856833';
 
 class CoinData {
   //TODO 3: Update getCoinData to take the selectedCurrency as an input.
-  Future getCoinData() async {
+  Future getCoinData(String selectedCurrency) async {
     //TODO 4: Update the URL to use the selectedCurrency input.
-    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    String requestURL = '$coinAPIURL/BTC$selectedCurrency';
     http.Response response = await http.get(requestURL);
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
--- bitcoin-flutter-silver-start/lib/price_screen.dart
+++ bitcoin-flutter-silver-start/lib/price_screen.dart
@@ -10,7 +10,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   //TODO 6: Update the default currency to AUD, the first item in the currencyList.
-  String selectedCurrency = 'USD';
+  String selectedCurrency = 'AUD';
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -29,6 +29,7 @@ class _PriceScreenState extends State<PriceScreen> {
         setState(() {
           //TODO 2: Call getData() when the picker/dropdown changes.
           selectedCurrency = value;
+          getData();
         });
       },
     );
@@ -46,7 +47,9 @@ class _PriceScreenState extends State<PriceScreen> {
       onSelectedItemChanged: (selectedIndex) {
         print(selectedIndex);
         //TODO 1: Save the selected currency to the property selectedCurrency
+        selectedCurrency = currenciesList[selectedIndex];
         //TODO 2: Call getData() when the picker/dropdown changes.
+        getData();
       },
       children: pickerItems,
     );
@@ -56,7 +59,7 @@ class _PriceScreenState extends State<PriceScreen> {
 
   void getData() async {
     try {
-      double data = await CoinData().getCoinData();
+      double data = await CoinData().getCoinData(selectedCurrency);
       setState(() {
         bitcoinValue = data.toStringAsFixed(0);
       });
@@ -93,7 +96,7 @@ class _PriceScreenState extends State<PriceScreen> {
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
                   //TODO 5: Update the currency name depending on the selectedCurrency.
-                  '1 BTC = $bitcoinValue USD',
+                  '1 BTC = $bitcoinValue $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
