b156e374cec07d71d9d32bcd0339d9b2128a7c2e
jackjackE <tmddjs210@naver.com>
2022-05-22 16:09:46 +0900

    164. Level 1 - Bronze Medal

--- bitcoin-flutter-challenge-start/lib/coin_data.dart
+++ bitcoin-flutter-challenge-start/lib/coin_data.dart
@@ -1,4 +1,6 @@
 //TODO: Add your imports here.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 const List<String> currenciesList = [
   'AUD',
@@ -34,5 +36,17 @@ const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
 const apiKey = 'YOUR-API-KEY-HERE';
 
 class CoinData {
+  String coinRateUrl = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+
   //TODO: Create your getCoinData() method here.
+  Future getCoinData() async {
+    http.Response response = await http.get(coinRateUrl);
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+    }
+  }
 }
--- bitcoin-flutter-challenge-start/lib/price_screen.dart
+++ bitcoin-flutter-challenge-start/lib/price_screen.dart
@@ -10,6 +10,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  String coinRate = '?';
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -49,11 +50,22 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   //TODO: Create a method here called getData() to get the coin data from coin_data.dart
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+      setState(() {
+        coinRate = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     //TODO: Call getData() when the screen loads up.
+    getData();
   }
 
   @override
@@ -78,7 +90,7 @@ class _PriceScreenState extends State<PriceScreen> {
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
                   //TODO: Update the Text Widget with the live bitcoin data here.
-                  '1 BTC = ? USD',
+                  '1 BTC = $coinRate USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
