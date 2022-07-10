10dd6d9bfc8425aa4f1fe7949998fd6d6a794bb3
jackjackE <tmddjs210@naver.com>
2022-07-10 15:21:11 +0900

    174. Custom Flutter Animations with the Animation Controller

--- flash-chat-flutter/lib/screens/welcome_screen.dart
+++ flash-chat-flutter/lib/screens/welcome_screen.dart
@@ -9,11 +9,39 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+// SingleTickerProviderStateMixin 는 AnimationController 를 사용하는데 필요
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+
+    controller.forward();
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -25,9 +53,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 Hero(
                   tag: 'logo',
                   child: Container(
-                    child: Image.asset('images/logo.png'),
-                    height: 60.0,
-                  ),
+                      child: Image.asset('images/logo.png'), height: 60.0),
                 ),
                 Text(
                   'Flash Chat',
